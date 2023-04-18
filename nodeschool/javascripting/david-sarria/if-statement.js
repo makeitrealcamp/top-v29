24e01043d349377b3160f0093ff3df12054eabe0
David Sarria <david.sarriav@gmail.com>
2023-04-18 23:34:17 +0200

    Add exercise if-statement

--- nodeschool/javascripting/david-sarria/if-statement.js
+++ nodeschool/javascripting/david-sarria/if-statement.js
@@ -1,7 +1,7 @@
 let fruits = 'orange';
 
 if (fruits.length > 5) {
-    console.log('The fruit name has more than five characters');
+    console.log('The fruit name has more than five characters.');
 } else {
-    console.log('The fruit name has five characters or less');
+    console.log('The fruit name has five characters or less.');
 }
