7ff3e7878ec61de54bd14fb3a49518f5766c8fcb
khriztianmoreno <khriztianmoreno@gmail.com>
2023-04-18 14:25:17 -0500

    feat: :sparkles: add random names

---
+++ examples/random/index.js
@@ -0,0 +1,18 @@
+const makersName = [
+  'Andres López',
+  'David Sarria Villada',
+  'Alexander Herrera',
+  'Juan Xavier Cabello Salirrosas',
+  'Federico Córdoba',
+  'Juan David Peña',
+  'Michael Gonzalez',
+  'Stephanie Ariza Luna',
+  'Miguel Angel Chaparro Tirado',
+  'Carlos Alfredo Mendoza Páez',
+];
+
+function randomMaker() {
+  return makersName[Math.floor(Math.random() * makersName.length)];
+}
+
+console.log('El turno es para: ', randomMaker());
