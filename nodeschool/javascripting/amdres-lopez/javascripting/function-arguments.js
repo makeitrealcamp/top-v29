4212d7a6e8401f5a099c05b9ede6bef4a5189770
Andres Lopez <73659007+maitahlcor@users.noreply.github.com>
2023-04-18 20:28:45 -0500

    [javascripting] andres lopez (#6)

---
+++ nodeschool/javascripting/amdres-lopez/javascripting/function-arguments.js
@@ -0,0 +1,6 @@
+
+function math (Arg1, Arg2, Arg3) {
+    return Arg2*Arg3 + Arg1
+  }
+
+  console.log(math(53, 61, 67))
---
+++ nodeschool/javascripting/amdres-lopez/javascripting/revising-strings.js
@@ -0,0 +1,3 @@
+const pizza = "pizza is alright";
+
+console.log(pizza.replace("alright", "wonderful"));
