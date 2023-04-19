0c832535eed5bc514daf93caedda2eeab89f172f
miguelangelchaparrotirado@gmail.com <miguelangelchaparrotirado@gmail.com>
2023-04-19 16:25:10 -0500

    6.2

--- nodeschool/learnyounode/miguel-chaparro/6.2-mymodule.js
+++ nodeschool/learnyounode/miguel-chaparro/6.2-mymodule.js
@@ -2,11 +2,11 @@ let fs = require('fs');
 let path = require('path');
 
 function modulo2(dir, ext, callback) {
-  var arr = [];
+  const arr = [];
   fs.readdir(dir, function (error, data) {
     if (error) return callback(error);
     data.forEach(function (file) {
-      if (path.extname(file) === ext)
+      if (path.extname(file) === `.${ext}`)
         arr.push(file)
     })
     callback(null, arr);
